8477016878f28407f197fbe4570711782624cc54
Mayur Srivastav <mayursrivastava877@gmail.com>
2024-10-17 21:45:47 +0530

    Learned about container, borderRadius, borderDecoration, and,  boxShadow

--- lib/widgets/container_sized.dart
+++ lib/widgets/container_sized.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
 
 class Container_Sized extends StatelessWidget {
-  const Container_Sized({super.key});
+  Container_Sized({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
         title: Text('Container and SizedBox'),
+      ),
+      body: Center(
+        child: Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                //shape: BoxShape.circle,
+                //borderRadius: BorderRadius.circular(20)
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20)),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 20,
+                    spreadRadius: 10,
+                    color: const Color.fromARGB(255, 27, 9, 39),
+                  )
+                ]),
+            child:
+                Center(child: Text("Hello", style: TextStyle(fontSize: 20)))),
+
+        // child: SizedBox(
+        //   height: 50,
+        //   width: 50,
+        //   child: Text("Hello"),
+        // ),
       ),
     );
   }
