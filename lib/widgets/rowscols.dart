58585079572238f8d5d6fc0bd6b662fcea5c88a9
Mayur Srivastav <mayursrivastava877@gmail.com>
2024-10-18 14:39:47 +0530

    Learned about Rows, Colums, MainAxisAlignment, CrossAxisAlignment, Wrap, MediaQuery

--- lib/widgets/rowscols.dart
+++ lib/widgets/rowscols.dart
@@ -5,10 +5,50 @@ class RowsCols extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var w = MediaQuery.of(context).size.width;
+    var h = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Rows and Columns"),
-      ),
+      appBar: AppBar(title: Text('Rows and Columns')),
+      body: Container(
+          height: h,
+          width: w,
+          color: Colors.yellow,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(height: 60, width: 60, color: Colors.red),
+                Container(height: 60, width: 60, color: Colors.blue),
+                Container(height: 60, width: 60, color: Colors.green),
+                Container(height: 60, width: 60, color: Colors.black),
+                Container(height: 60, width: 60, color: Colors.purple),
+              ])
+          //  Wrap(
+          //   direction: Axis.vertical,
+          //   alignment: WrapAlignment.start,
+          //   // mainAxisAlignment: MainAxisAlignment.start,
+          //   // crossAxisAlignment: CrossAxisAlignment.center,
+          //   children: [
+          // Container(height: 60, width: 60, color: Colors.red),
+          // Container(height: 60, width: 60, color: Colors.blue),
+          // Container(height: 60, width: 60, color: Colors.green),
+          // Container(height: 60, width: 60, color: Colors.black),
+          // Container(height: 60, width: 60, color: Colors.purple),
+          //   ],
+          // ),
+          //     Row(
+          //   mainAxisAlignment: MainAxisAlignment.start,
+          //   crossAxisAlignment: CrossAxisAlignment.center,
+          //   children: [
+          //     Container(height: 60, width: 60, color: Colors.red),
+          //     Container(height: 60, width: 60, color: Colors.blue),
+          //     Container(height: 60, width: 60, color: Colors.green),
+          //     Container(height: 60, width: 60, color: Colors.black),
+          //     Container(height: 60, width: 60, color: Colors.purple),
+          //   ],
+          // )
+          ),
     );
   }
 }
