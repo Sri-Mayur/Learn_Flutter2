a21791428b8a19b7020c3f136f6d74ebf40ec430
Mayur Srivastav <mayursrivastava877@gmail.com>
2024-10-18 14:11:35 +0530

    Initialized a Stateless widget named RowsCols

---
+++ lib/widgets/rowscols.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RowsCols extends StatelessWidget {
+  const RowsCols({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Rows and Columns"),
+      ),
+    );
+  }
+}
