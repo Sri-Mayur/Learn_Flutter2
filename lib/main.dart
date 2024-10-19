670775b574bde71ee6d7644c40077bef85efa76b
Mayur Srivastav <mayursrivastava877@gmail.com>
2024-10-19 18:48:58 +0530

    Changed home from ListGrid.dart to SnackBarWidget()

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:learning/widgets/button.dart';
 import 'package:learning/widgets/container_sized.dart';
 import 'package:learning/widgets/list_grid.dart';
 import 'package:learning/widgets/rowscols.dart';
+import 'package:learning/widgets/snackbar.dart';
 
 void main() => runApp(new MyApp());
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           brightness: Brightness.dark,
           primaryColor: const Color.fromARGB(255, 68, 36, 122)),
-      home: ListGrid(),
+      home: SnackbarWidget(),
     );
   }
 }
